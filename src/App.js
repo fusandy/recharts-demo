54aa00dbac471dd13dd7bdce63d48a348fb2089e
Sandy Fu <sandy.fu@idcservices.net>
2024-03-11 12:06:05 +0800

    0311 customize tooltip

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import { data } from './mockData';
 
+// XAxis props
+// {
+//   "textAnchor":"middle",
+//   "verticalAnchor":"start",
+//   "angle":0,
+//   "orientation":"bottom",
+//   "width":1240,
+//   "height":30,
+//   "x":1300,
+//   "y":261,
+//   "className":"recharts-xAxis xAxis",
+//   "stroke":"none",
+//   "fill":"#666",
+//   "index":24,
+//   "payload":{
+//     "coordinate":1300,
+//     "value":"24:00",
+//     "index":24,
+//     "offset":0,
+//     "tickCoord":1300,
+//     "isShow":true
+//   },
+//   "visibleTicksCount":25
+// }
+
 const App = () => {
   return (
     <ResponsiveContainer width='100%' height={275}>
@@ -29,45 +54,18 @@ const App = () => {
             tickLine={true} // 刻度
             tickMargin={10} // 刻度數值 margin top
             minTickGap={30} // 刻度之間最小的間隔寬度
-            tick={props => {
-              // console.log(`props: ${JSON.stringify(props)}`)
-              const { x, y, payload, index } = props
-              // {
-              //   "textAnchor":"middle",
-              //   "verticalAnchor":"start",
-              //   "angle":0,
-              //   "orientation":"bottom",
-              //   "width":1240,
-              //   "height":30,
-              //   "x":1300,
-              //   "y":261,
-              //   "className":"recharts-xAxis xAxis",
-              //   "stroke":"none",
-              //   "fill":"#666",
-              //   "index":24,
-              //   "payload":{
-              //     "coordinate":1300,
-              //     "value":"24:00",
-              //     "index":24,
-              //     "offset":0,
-              //     "tickCoord":1300,
-              //     "isShow":true
-              //   },
-              //   "visibleTicksCount":25
-              // }
-              return (
+            tick={({ x, y, payload, index  }) => (
                 <g transform={`translate(${x},${y})`}>
                   <text x={0} y={0} dy={10} fill='#858585'>
-                    <tspan textAnchor='middle' x='0' style={{ fontSize: '12px', letterSpacing: '0.6px' }}>
+                    <tspan textAnchor='middle' x='0' style={{ fontSize: '12px' }}>
                       {data[index].date}
                     </tspan>
-                    <tspan textAnchor='middle' x='0' dy='20' style={{ fontSize: '12px', letterSpacing: '0.6px' }}>
+                    <tspan textAnchor='middle' x='0' dy='20' style={{ fontSize: '12px' }}>
                       {payload.value}
                     </tspan>
                   </text>
                 </g>
-              )
-            }} // 客製刻度值 
+            )} // 客製刻度值 
           />
           <YAxis
             dataKey='usage'
@@ -76,13 +74,10 @@ const App = () => {
             tickLine={false}
             tickMargin={20}
             tickCount={6}
-            tick={props => {
-              // console.log(`props: ${JSON.stringify(props)}`)
-              const { x, y, index, payload } = props
-              return (
-                <>
+            tick={({ x, y, index, payload }) => (
+              <>
                 {index !== 0 && (
-                  <g transform={`translate(${x},${y - 15})`}>
+                  <g transform={`translate(${x}, ${y - 15})`}>
                     <text x={0} y={0} dy={10} fill='#858585'>
                       <tspan textAnchor='middle' x='0' dy='20' style={{ fontSize: '12px', letterSpacing: '0.6px' }}>
                         {`${payload.value}%`}
@@ -90,9 +85,8 @@ const App = () => {
                     </text>
                   </g>
                 )}
-                </>
-              )
-            }}
+              </>
+            )}
           />
           <CartesianGrid
             strokeDasharray='0'
@@ -100,7 +94,23 @@ const App = () => {
             stroke='#EBEBEB'
             strokeWidth='0.5px'
           />
-          <Tooltip />
+          <Tooltip
+            offset={10}
+            cursor={{ stroke: '#D2D0D0', strokeWidth: 1, strokeDasharray: 3 }}
+            content={({ active, payload }) => {
+              const { date, time, usage } = payload[0]?.payload || {}
+              return active && payload && payload.length && (
+                <div style={{ backgroundColor: '#1F2B50', color: '#fff', borderRadius: '10px', padding: '10px 20px' }}>
+                  <div style={{ minHeight: '23px', fontSize: '20px', fontWeight: '800', textAlign: 'center' }}>
+                    {`${usage}%`}
+                  </div>
+                  <div style={{ fontSize: '10px', lineHeight: '180%' }}>
+                    {date} {time}
+                  </div>
+                </div>
+              )
+            }}
+          />
           <Area
             type='monotone'
             dataKey='usage'
